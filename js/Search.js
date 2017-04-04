381701dc648990189fc57fd9c751ce99d514b230
lillyanka <sittingducks@gmail.com>
2017-04-03 22:11:02 -0600

    show cards

---
+++ js/Search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='search'>
+        {preload.shows.map((show) => {
+          return (
+            <ShowCard key={show.imdbID} show={show} />
+          )
+        })}
+      </div>
+    )
+  }
+})
+
+export default Search
