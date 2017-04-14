94352fee46d60854d5c1fc0cbd8077a661573248
Lilliana Viquez Murillo <sittingducks@gmail.com>
2017-04-13 22:14:45 -0600

    testing with enzyme and Istanbul

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import Search from './Search'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
+import Search from './Search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
 
 test('Search snapshot test', () => {
   const component = shallow(<Search />)
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () =>{
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+  //expect the amount of ShowCards in <Search /> to be the same as the amount of shows in `preload`
+})
+
+test('Search should render correct amount of shows based on search term', () => {
+  const searchWord = 'House'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
+  expect(component.find(ShowCard).length).toEqual(showCount)
 })
